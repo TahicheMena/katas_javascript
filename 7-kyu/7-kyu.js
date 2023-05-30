9279ad7ef0f48a3939208caa42337f814d80adff
Tahiche <tahichemm@gmail.com>
2023-05-30 20:56:32 +0100

    Se elimina kata repetido y me quedo con la versión más compacta

--- 7-kyu/7-kyu.js
+++ 7-kyu/7-kyu.js
@@ -1,27 +1,4 @@
 function getAVG(arrayOfNumbers) {
-  if (!Array.isArray(arrayOfNumbers)) {
-    return 0;
-  }
-  if (arrayOfNumbers.length === 0) {
-    return 0;
-  }
-
-  let avg;
-  let sumNumbers = 0;
-  let countNumbers = 0;
-
-  for (const number of arrayOfNumbers) {
-    if (typeof number === 'number') {
-      sumNumbers += number;
-      countNumbers++;
-    }
-  }
-  avg = sumNumbers / countNumbers;
-
-  return avg;
-}
-
-function getAVGAdvanced(arrayOfNumbers) {
   if (!Array.isArray(arrayOfNumbers) || arrayOfNumbers.length === 0) {
     return 0;
   }
@@ -35,4 +12,4 @@ function getAVGAdvanced(arrayOfNumbers) {
   return sum / arrayOfRealNumbers.length;
 }
 
-module.exports = { getAVG, getAVGAdvanced };
+module.exports = { getAVG };
--- 7-kyu/7-kyu.test.js
+++ 7-kyu/7-kyu.test.js
@@ -7,7 +7,6 @@ const {
   buildArray,
   findDuplicates,
   removeValues,
-  getAVGAdvanced,
 } = require('./7-kyu');
 
 /**
@@ -23,14 +22,6 @@ it("getAVG", () => {
   expect(getAVG([1,'lol',null, null, 3])).toEqual(2);
   expect(getAVG(null)).toEqual(0);
   expect(getAVG([])).toEqual(0);
-});
-it("getAVGAdvanced", () => {
-  expect(getAVGAdvanced([1,3,6,2])).toEqual(3);
-  expect(getAVGAdvanced([1,1,1,1,1,1,1])).toEqual(1);
-  expect(getAVGAdvanced([1,'lol',3])).toEqual(2);
-  expect(getAVGAdvanced([1,'lol',null, null, 3])).toEqual(2);
-  expect(getAVGAdvanced(null)).toEqual(0);
-  expect(getAVGAdvanced([])).toEqual(0);
 });
 
 
